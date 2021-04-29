84a4bdacd8565d19b94e9c088946712d8db30e8a
itunes2412 <itunes2412@gmail.com>
2021-04-28 22:24:42 -0500

    subiendo cambios

--- src/App.js
+++ src/App.js
@@ -5,66 +5,187 @@ function App() {
   return (
    <>
     <header>
+      
+
       <nav>
         <div className="logo">
           <img src="img/Mueblerias.png">
           </img>
         </div>
+        
+        
         <div className="menu">
           <a href="#">
-              Inicio
+            INICIO
           </a>
           <a href="#">
-           Pantallas
+           PANTALLAS
           </a>
           <a href="#">
-            Estereos
+            AUDIO
           </a>
           <a href="#">
-            Tablets
+            TORNAMESAS
           </a>
           <a href="#">
-            Celulares
+            CELULARES
           </a>
           <a href="#">
-           Computadoras
+           COMPUTADORAS
           </a>
           <a href="#">
-            Contacto
+            CONTACTO
           </a>
-
+    
 
         </div>
       </nav>
       <div className="cabecera">
-      <img  src="img/Uno.png">
+      <img  src="img/top.jpg">
       </img>
       </div>
     </header>
-    <section>
-      
-         <h2>
-             
-        </h2>
+    
 
-      <div className="informacion">
+    <section>
+
+          <div className="informacion">
           <div className="informacion_imagen">
+             
               <img src="img/electro.png">
               </img>
-          </div>
-          <div className="informacion_texto">
-              <p align="justify">
-              En Muebles Troncaso encontrarás los precios más baratos del mercado, con una selección de electrodomésticos de gran calidad. Te ofrecemos las mejores marcas, tales como Bosch, Balay, Samsung, Siemens,Teka, Electrolux, AEG, LG y otras muchas que se encuentran entre las más reconocidas y prestigiosas del mundo de los electrodomésticos.
-              En nuestra tienda online te ponemos al alcance un amplio catálogo de electrodomésticos baratos para que puedas escoger el que más te guste al mejor precio del momento.
-              Compra de forma sencilla, rápida y económica los electrodomésticos que necesites para tu hogar y equipa toda tu casa con los electrodomésticos baratos de calidad que estás buscando.
 
-              </p>
           </div>
+          
+	<div class="column">
+		<h2>TODAS LAS MARCAS DE RENOMBRE EN UN SOLO SITIO</h2>
+		<p>En Muebles Troncaso encontrarás los precios más bajos del mercado, con una selección de electrodomésticos de 
+              gran calidad. Te ofrecemos las mejores marcas, tales como Bosch, Stanton, Balay, Samsung, Siemens, Teka, Sony, 
+              BOSE, Electrolux, AEG, LG y otras muchas que se encuentran entre las más reconocidas y prestigiosas del mundo 
+              de los electrodomésticos.
+              En nuestra tienda online te ponemos al alcance un amplio catálogo de electrodomésticos baratos para que puedas 
+              escoger el que más te guste al mejor precio del momento.
+              Compra de forma sencilla, rápida y económica los electrodomésticos que necesites para tu hogar y equipa toda tu 
+              casa con los electrodomésticos baratos de calidad que estás buscando.</p>
+	</div>
+	
       </div>
+
     </section>
+
+    <section>
+    <div className="catalago">
+    <div className="catalago_imagen">
+  
+  <img src="img/computadora.png"></img>
+
+  </div>
+  </div>
+
+    </section>
+
+    
+    <div class="wrapper">
+
+	<header>
+		<h1>COMPUTADORAS 2021</h1>
+	</header>
+
+  <section>
+    <div className="lap">
+    <div className="lap_imagen">
+  
+    <img src="img/asuslap.png"></img>
+    <img src="img/asuslapII.png"></img>
+    <img src="img/asuslapIII.png"></img>
+
+  </div>
+  </div>
+
+    </section>
+
+		
+<section class="columns">
+
+	<div class="column">
+		<h2>Laptop Gamer Asus TUF 15.6"</h2>
+		<p>Con la tecnología del procesador Intel Core i5 de décima generación, de 4 núcleos, y la GPU GeForce GTX 1650 Ti, 
+      tu juego lleno de acción será rápido y fluido, y podrás disfrutar de tu pantalla de nivel IPS con tasa de actualización
+       de 60Hz. Aún con un chasis más pequeño y portátil que sus predecesores, esta laptop gamer también te da un eficiente 
+       enfriamiento auto limpiante que se combina con la durabilidad de grado militar característica de TUF, almacenamiento 
+       híbrido y 8G de RAM, expandibles a 32G para hacer de esta guerrera probada en batalla, una poderosa aliada para cualquier jugador.</p>
+	</div>
+	
+	<div class="column">
+		<h2>ZenBook Duo 14" (UX482)</h2>
+		<p>ZenBook Duo 14 te permite hacer todo con calma, eficiencia y mucho estilo. Es una potente y elegante laptop creado para
+       ayudarte a hacer con facilidad tus proyectos creativos y tareas de productividad. La pantalla secundaria ScreenPad™ Plus
+        presenta un diseño reclinable que mejora su ergonomía y acelera tu flujo de trabajo. Equipado con los nuevos procesadores 
+        Intel® Core™ de 11.ª Gen., el ZenBook Duo 14 te permite disfrutar hoy de la tecnología del mañana.</p>
+	</div>
+  
+  <div class="column">
+		<h2>ASUS VivoBook Flip 14 TP412</h2>
+		<p>ASUS VivoBook Flip 14 es una laptop convertible que cuenta con el bisel ASUS NanoEdge extremadamente delgado, lo que permite
+       que su pantalla Full HD de 14 pulgadas se ajuste a un marco típico de una laptop de 13 pulgadas. La delgada y liviana VivoBook 
+       Flip 14 está diseñada para adaptarse a cualquier ocasión. Usala como una elegante computadora portátil, una práctica tableta, o 
+       cualquier otra cosa. Su procesador Intel® Core ™ i5 te permite navegar a través de las tareas cotidianas con facilidad.</p>
+	</div>
+  
+	
+</section>	
+<section class="columns">
+
+	<div class="column">
+		<h2>Lorem ipsum dolor sit amet consectetur adipiscing elit interdum leo ante, dis gravida sagittis vestibulum quisque odio ad praesent fames. Feugiat diam maecenas ut malesuada nec libero a eros, interdum potenti gravida nunc neque cursus penatibus, netus magna dapibus curabitur litora natoque laoreet. Inceptos sodales hac dignissim erat proin vulputate ullamcorper maecenas, cras aptent gravida conubia rutrum ridiculus orci aliquet dictumst, rhoncus lacus justo vitae platea eu mollis.</h2>
+    
+		<p>Lorem ipsum dolor sit amet consectetur adipiscing elit interdum leo ante, dis gravida sagittis vestibulum quisque odio ad praesent fames. Feugiat diam maecenas ut malesuada nec libero a eros, interdum potenti gravida nunc neque cursus penatibus, netus magna dapibus curabitur litora natoque laoreet. Inceptos sodales hac dignissim erat proin vulputate ullamcorper maecenas, cras aptent gravida conubia rutrum ridiculus orci aliquet dictumst, rhoncus lacus justo vitae platea eu mollis.
+
+Feugiat gravida egestas eu sociis consequat imperdiet conubia quisque scelerisque, convallis lectus aliquet aptent dis urna vestibulum nullam mus velit, auctor tortor varius venenatis tempor lobortis sodales proin. Nec ridiculus cum platea taciti curae metus urna, feugiat bibendum quisque integer blandit massa morbi quis, ullamcorper rutrum fusce sollicitudin elementum sagittis. In arcu suscipit magna libero quis porttitor aptent sed vehicula, nostra etiam bibendum sapien sociis purus inceptos nam montes, mi a tempus quam sodales magnis luctus cras.</p>
+	</div>
+	
+	
+  
+	
+</section>	
+	
+	</div>
+  
+
+<section><h4>ENTRAR</h4></section>
+  
+  <div class="login wrap">
+  <input type="text" name="email" id="email" placeholder="Correo Electrónico*" pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"/>
+  <input type="password" name="password" id="password" placeholder="Contraseña*" />
+  <input type="button" value="Ayuda" />
+  <input type="submit" value="Iniciar Sesión" />
+</div>
+
+
+
+
+
+<section>
+    <div className="catalago">
+    <div className="catalago_imagen">
+  
+  <img src="img/barra.png"></img>
+
+  </div>
+  </div>
+
+    </section>
+
+
+
+
     <footer>
+  <div className="logom">
+  <img src="img/logo.png"></img>
+  </div>
+
       <div className="telefonos">
-        <h3>Servicio al Cliente</h3>
+        <h3>Soporte</h3>
         <p>8000213000</p>
         <p>3312587421</p>
         
@@ -78,7 +199,7 @@ function App() {
         <h3>Sucursales</h3>
         <p>Guadalajara</p>
         <p>CDMX</p>
-        <p>Monterrey</p>
+       
       </div>
       <div className="redes">
           <img src="img/face.png"></img>
@@ -86,6 +207,7 @@ function App() {
           <img src="img/twit.png"></img>
           <img src="img/you.png"></img>
       </div>
+      <h6>©2021 Muebles Troncaso</h6>
     </footer>
    </>
   );
